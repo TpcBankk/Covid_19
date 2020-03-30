bf1f92cc04dbb92a615bdc6853788728dd106e49
TpcBankk <55575947+TpcBankk@users.noreply.github.com>
2020-03-30 17:31:50 +0700

    Add Button Login Google Account

--- app/src/main/java/com/example/covid_19/Login.java
+++ app/src/main/java/com/example/covid_19/Login.java
@@ -1,6 +1,7 @@
 package com.example.covid_19;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,6 +14,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,25 +29,30 @@ import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
+    public static final int GOOGLE_SIGN_IN_CODE = 10005;
     EditText mEmail,mPassword;
     Button mLoginBtn;
     TextView mCreateBtn;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
 
+    // SignIn Google Account
+    SignInButton signIn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient signInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        //login normal
         mEmail = findViewById(R.id.Email);
         mPassword = findViewById(R.id.Password);
         progressBar = findViewById(R.id.progressBar);
         fAuth = FirebaseAuth.getInstance();
         mLoginBtn = findViewById(R.id.loginBtn);
         mCreateBtn = findViewById(R.id.createText);
-
 
         //คลิกปุ่ม Login
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,9 +70,6 @@ public class Login extends AppCompatActivity {
                     return;
                 }
 
-
-
-
                 fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -76,13 +85,48 @@ public class Login extends AppCompatActivity {
             }
 
         });
-
+        //back to register page
         mCreateBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 startActivity(new Intent(getApplicationContext(),Register.class));
             }
         });
+
+        //login google account
+        signIn = findViewById(R.id.signIn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        signInClient = GoogleSignIn.getClient(this,gso);
+
+        final GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            startActivity(new Intent(this,MainActivity.class));
+        }
+
+        signIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent sign = signInClient.getSignInIntent();
+                startActivityForResult(sign, GOOGLE_SIGN_IN_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GOOGLE_SIGN_IN_CODE){
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+                GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
+                Toast.makeText(this, "Your Google Account is Connected to our Application", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,MainActivity.class));
+            } catch (ApiException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.covid_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +23,24 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_LONG).show();
     }
 
-    public void logout(View view){
+    public void logout(final View view){
         FirebaseAuth.getInstance().signOut(); //logout
-        startActivity(new Intent(getApplicationContext() , Login.class));
+        startActivity(new Intent(getApplicationContext() , Register.class));
         finish();
+
+        //logout Google
+        GoogleSignIn.getClient(this,new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()).
+                signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(view.getContext(),Login.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Signout Failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
