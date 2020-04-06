f14ae350e82bb361b87b64a0c1478f494510e2fc
TpcBankk <55575947+TpcBankk@users.noreply.github.com>
2020-04-06 20:13:57 +0700

    app Menu(fragment) v.1.2

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,6 +12,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.service.dreams.DreamService;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -22,7 +24,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, MainFragment.onFragmentBtnSelected{
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -60,13 +62,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
+        drawerLayout.closeDrawer(GravityCompat.START );
         if (menuItem.getItemId() == R.id.home){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.contener_fragment,new MainFragment());
+            fragmentTransaction.commit();
 
         }
-        if (menuItem.getItemId() == R.id.home){
-
+        if (menuItem.getItemId() == R.id.another){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.contener_fragment,new FragmentSecond());
+            fragmentTransaction.commit();
         }
         return true;
+    }
+
+    @Override
+    public void onButtonSelected() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contener_fragment,new FragmentSecond());
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/covid_19/MainFragment.java
+++ app/src/main/java/com/example/covid_19/MainFragment.java
@@ -1,10 +1,12 @@
 package com.example.covid_19;
 
+import android.content.Context;
 import android.net.Uri;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
+    private onFragmentBtnSelected listener;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup comtainer, @Nullable Bundle saveInstanceState){
-        View view = inflater.inflate(R.layout.fragment_main,comtainer,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        Button clickme = view.findViewById(R.id.load);
+        clickme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onButtonSelected();
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof onFragmentBtnSelected){
+
+        }else{
+            throw new ClassCastException(context.toString() + "Must implement listener");
+        }
+        listener = (onFragmentBtnSelected) context;
+    }
+
+    public interface onFragmentBtnSelected{
+        public void onButtonSelected();
     }
 }
