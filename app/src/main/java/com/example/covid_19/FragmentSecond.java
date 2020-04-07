19d837d4c0d37ed9968a1af52c10da72a27771bc
Dreamsoobinms <55575873+Dreamsoobinms@users.noreply.github.com>
2020-04-07 16:50:34 +0700

    test covid qiuz

--- app/src/main/java/com/example/covid_19/FragmentSecond.java
+++ app/src/main/java/com/example/covid_19/FragmentSecond.java
@@ -1,10 +1,13 @@
 package com.example.covid_19;
 
 import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 import androidx.annotation.NonNull;
@@ -16,6 +19,14 @@ public class FragmentSecond extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.fragment_second,container,false);
+        Button buttonStartQuiz = view.findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
